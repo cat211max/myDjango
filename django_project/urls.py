5b5dc37b6052bd829e3bc48d50e11c2a99aed13f
HowardYao <yidanyao@gmail.com>
2025-12-07 00:59:34 +0800

    add the posts app

--- django_project/urls.py
+++ django_project/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include("posts.urls")), 
     path("", include("pages.urls")), 
+    
 ]
